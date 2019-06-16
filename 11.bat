74e302010ca51bd6ea4d0b1f0362129a84d6890f
lee-lol <51871188+lee-lol@users.noreply.github.com>
2019-06-16 12:31:37 +0800

    Add files via upload

---
+++ 11.bat
@@ -0,0 +1,7 @@
+.\bin\convert_mnist_data.exe .\data\mnist\mnist_train_lmdb\train-images-idx3-ubyte .\data\mnist\mnist_train_lmdb\train-labels-idx1-ubyte .\examples\mnist\mnist_train_lmdb 
+
+echo. 
+
+.\bin\convert_mnist_data.exe .\data\mnist\mnist_test_lmdb\t10k-images-idx3-ubyte .\data\mnist\mnist_test_lmdb\t10k-labels-idx1-ubyte .\examples\mnist\mnist_test_lmdb
+
+pause
---
+++ 44.bat
@@ -0,0 +1,2 @@
+.\bin\convert_imageset.exe --gray=true .\datasets\train .\datasets\train.txt .\datasets\fer2013_train_lmdb
+pause
